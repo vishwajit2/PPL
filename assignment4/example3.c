e8e789e5eea4554d22faaf707f79a6d8e16fc5c6
vishwajit2 <vishwajitkadam13@gmail.com>
2020-11-11 12:44:05 +0530

    added assgn 4

---
+++ assignment4/example3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void function(int a, int b, int c)
+{
+    char buffer1[5];
+    char buffer2[10];
+    int *ret;
+    ret = buffer1 + 21;
+    (*ret) += 7;
+}
+void main()
+{
+    int x;
+    x = 0;
+    function(1, 2, 3);
+    x = 1;
+    printf("%d\n", x);
+}
---
+++ assignment4/sp.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned long get_sp(void) {
+	__asm__("movl %esp,%eax");
+}
+
+void main() {
+	printf("0x%x\n", get_sp());
+}
